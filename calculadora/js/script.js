3140e9001e15924a585a563d63652a4cc1cb5839
Lirryaynna <aynavalentina@gmail.com>
2024-10-17 15:23:53 +0000

    criadas as quatro operaçaos

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,4 +1,5 @@
 // FAZ REFERÊNCIA AOS ELEMENTOS DOM
+
 const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
 const btnCalcular = document.querySelector('#btn-calcular')
@@ -10,24 +11,33 @@ const divisao = document.querySelector('.divisao')
 
 //alert(numero1)
 // CRIA A FUNÇÃO CALCULAR
-function calcular() {
-   
-    cost n1 =Number ( numero1.value)
-    cost n2 =Number ( numero2.value)
-    if( typeof n1=== 'number'
-        && typeof n2 === 'number') {
-             cost adicao = " a soma de $ {n1} e $ { n2} = $ {n1+n2}"
-             alert ( adicao)
+function calcular(){
+    const n1 = Number(numero1.value)
+    const n2 = Number(numero2.value)
+    if(typeof n1 === 'number'
+        && typeof n2 === 'number' ) {
+          const add =  `A soma de ${n1} e ${n2} = ${n1+n2}` 
+          //alert(add)
+          adicao.innerHTML=add
+
+const sub =  ` A soma de ${n1} e ${n2} = ${n1+n2}`
+          subtracao.innerHTML=add
+
+const mult =  ` A soma de ${n1} e ${n2} = ${n1+n2}`
+          multiplicacao.innerHTML=add
+
+ const div =  `A soma de  ${n1} e ${n2} = ${n1+n2}`
+          divisao.innerHTML=add
+
+
         } else {
-            alert( 'por favor , digite um numero correto ')
+            alert('Por favor, digite um número correto')
         }
-  }
-    
-
-
+   
+    //alert('Resultado: ' + resultado)
+}
 // ADICIONA ESCUTAR DE EVENTOS
-btnCalcular.addEventListener("click", function() {
-
-calcular()
-
-} )
+btnCalcular.addEventListener('click', function(evento){
+    evento.preventDefault() //tirar o comportamento padrao
+    calcular()
+})
